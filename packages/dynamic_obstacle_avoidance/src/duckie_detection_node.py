53e9995f5abdb66eee27e7d0a41ed39869747618
Ruben Viana <up202005108@up.pt>
2025-01-07 19:12:15 +0000

    updated code

--- packages/dynamic_obstacle_avoidance/src/duckie_detection_node.py
+++ packages/dynamic_obstacle_avoidance/src/duckie_detection_node.py
@@ -3,12 +3,11 @@ from cv_bridge import CvBridge, CvBridgeError
 from duckietown_msgs.msg import BoolStamped, Pixel, LanePose
 from duckietown_msgs.srv import ChangePattern
 from geometry_msgs.msg import Point
-from sensor_msgs.msg import CompressedImage, Image
+from sensor_msgs.msg import CompressedImage, Image, CameraInfo
 from std_msgs.msg import Float32, Float64MultiArray, String
+from image_geometry import PinholeCameraModel
 import cv2
 import numpy as np
-import os
-import rospkg
 import rospy
 import yaml
 from duckietown_utils import (logger, get_duckiefleet_root, load_camera_intrinsics, load_homography)
@@ -26,36 +25,31 @@ class DuckieDetectionNode(DTROS):
         self.active = True
         self.config = self.setupParam("~config", "baseline")
 
-        self.publish_debugimg = True #boolean to publish debug image (boundingbox and mask)
+        self.publish_duration = rospy.Duration.from_sec(1.0/2.0)
+        self.last_stamp = rospy.Time.now()
+
         self.lane_width = 0.1145 #half of lane
         self.crop_factor = 0.3 #percentage of image cropped from the top
         self.d = 0.0
         self.phi = 0.0
         self.duckie_pos_arr_prev = []
 
+        self.pcm = PinholeCameraModel()
+        # self.H = load_homography(rospy.get_namespace().strip("/"))  # load_homography not working! so hardcoded
+        self.H = np.array([
+            [-0.00015389980014132867, 0.00033474595519600086, 0.3198466654067245],
+            [-0.0013999151846608236, -1.316249553467081e-05, 0.43310887263768993],
+            [-0.00039075225726160825, 0.010959818387184139, -1.5504152912606086]
+        ])
 
         self.resolution=np.array([0,0])
         self.resolution[0]=rospy.get_param('/%s/camera_node/res_w' %rospy.get_namespace().strip("/"))
         self.resolution[1]=rospy.get_param('/%s/camera_node/res_h' %rospy.get_namespace().strip("/"))
 
-        try:
-            self.intrinsics = load_camera_intrinsics(rospy.get_namespace().strip("/"))
-            self.fx = self.intrinsics['K'][0][0]
-            self.fy = self.intrinsics['K'][1][1]
-        except KeyError as e:
-            rospy.logerr(f"Missing key in camera intrinsics: {e}")
-            self.intrinsics = {'K': [[0, 0, 0], [0, 0, 0], [0, 0, 0]], 'D': [0, 0, 0, 0, 0]}
-            self.fx = self.fy = 1.0
-        try:
-            self.H = load_homography(rospy.get_namespace().strip("/"))
-        except KeyError as e:
-            rospy.logerr(f"Missing key in homography: {e}")
-            self.H = np.eye(3)
-        self.Hinv = np.linalg.inv(self.H)
-
         #hsv range for yellow duckies
-        self.yellow_low = np.array([14, 100, 80])
-        self.yellow_high = np.array([35, 255, 255])
+        self.yellow_low = np.array([14, 100, 80]) # np.array([103, 140, 140])
+        self.yellow_high = np.array([35, 255, 255]) # np.array([123, 190, 190])
+
 
         self.sub_image = rospy.Subscriber("~image", CompressedImage, self.processImage,
                                             buff_size=921600, queue_size=1)
@@ -64,6 +58,10 @@ class DuckieDetectionNode(DTROS):
                                            self.cbSwitch, queue_size=1)
 
         self.sub_lane_pose = rospy.Subscriber("~lane_pose", LanePose, self.cbLanePose, queue_size=1)
+
+        self.sub_info = rospy.Subscriber(
+            "~camera_info", CameraInfo, self.cb_process_camera_info, queue_size=1
+        )
 
         self.pub_boundingbox_image = rospy.Publisher("~duckiedetected_image/compressed",
                                                        CompressedImage, queue_size=1)
@@ -76,9 +74,6 @@ class DuckieDetectionNode(DTROS):
         self.pub_duckie = rospy.Publisher("~detected_duckie",
                                                 dynamic_obstacle, queue_size=1)
 
-        # self.changePattern = rospy.ServiceProxy("", ChangePattern)
-        # self.last_led_state = None
-
         rospy.loginfo("[%s] Initialization completed" % (self.node_name))
 
     def setupParam(self, param_name, default_value):
@@ -89,6 +84,9 @@ class DuckieDetectionNode(DTROS):
 
     def cbSwitch(self, switch_msg):
         self.active = switch_msg.data
+
+    def cb_process_camera_info(self, msg):
+        self.pcm.fromCameraInfo(msg)
 
     def processImage(self, image_msg):
         if not self.active:
@@ -102,24 +100,14 @@ class DuckieDetectionNode(DTROS):
 
         start = rospy.Time.now()
 
-        # hsv_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-
-        # yellow_mask = cv2.inRange(hsv_img, self.yellow_low, self.yellow_high)
-
-        # #publish debug images
-        # self.pub_mask_image.publish(self.bridge.cv2_to_imgmsg(yellow_mask, "passthrough"))
-
-        # rospy.loginfo("[%s] Image processed" % (self.node_name))
-
-        # elapsed_time = (rospy.Time.now() - start).to_sec()
-        # self.pub_time_elapsed.publish(elapsed_time)
-
         #crop upper percentage and lower 1/4 of image
         cv_image_crop = cv_image[int(cv_image.shape[0]*self.crop_factor):int(cv_image.shape[0]*3/4)][:]
         hsv_img = cv2.cvtColor(cv_image_crop, cv2.COLOR_BGR2HSV)
 
         #create a yellow mask
         mask = cv2.inRange(hsv_img, self.yellow_low, self.yellow_high)
+
+        # TODO: improve duckie detection
 
         #initialize arrays
         duckiefound = False
@@ -138,7 +126,7 @@ class DuckieDetectionNode(DTROS):
         params.filterByColor = True
         params.blobColor = 255
         params.filterByArea = True
-        params.minArea = 50
+        params.minArea = 60
         params.filterByInertia = True
         params.minInertiaRatio = 0.5 #if high ratio: only compact blobs are detected, elongated blobs are filtered out
         params.filterByConvexity = False
@@ -169,7 +157,7 @@ class DuckieDetectionNode(DTROS):
                 # rospy.loginfo("[%s] ------- " % (self.node_name))
                 # rospy.loginfo("[%s] Duckie detected at (%f,%f) in pixel coordinates" % (self.node_name,duckie_loc_pix.u,duckie_loc_pix.v))
                 # rospy.loginfo("[%s] Duckie detected at (%f,%f) in world coordinates" % (self.node_name,duckie_loc_world.x,duckie_loc_world.y))
-                # rospy.loginfo("[%s] Duckie detected at %f m to the side of the middle line" % (self.node_name,duckie_side))
+                # rospy.loginfo("[%s] Duckie at p(%f,%f)   w[%f,%f]   %fm" % (self.node_name,duckie_loc_pix.u,duckie_loc_pix.v,duckie_loc_world.x,duckie_loc_world.y,duckie_side))
 
                 #tracking for outlier prevention: add duckie only if it has been detected already previously close by (within a range of 10cm)
                 # for a in range(len(self.duckie_pos_arr_prev)):
@@ -177,14 +165,14 @@ class DuckieDetectionNode(DTROS):
                 #     prev_detected.append([dist<0.1])
 
                 #check if duckie is on the left or right lane
-                if duckie_loc_pix.u >= 250: #right lane
+                if abs(duckie_side)<self.lane_width: #right lane
                     duckie_state_arr.append([1])
                     duckie_pos_arr.append([duckie_loc_world.x,duckie_loc_world.y])
-                    rospy.loginfo("[%s] Duckie detected on right lane" % (self.node_name))
-                elif duckie_loc_pix.u < 250: #left lane
+                    rospy.loginfo("[%s] Duckie on right lane at %fm" % (self.node_name, duckie_loc_world.x))
+                elif duckie_side>self.lane_width and duckie_side<self.lane_width*2: #left lane
                     duckie_state_arr.append([2])
                     duckie_pos_arr.append([duckie_loc_world.x,duckie_loc_world.y])
-                    rospy.loginfo("[%s] Duckie detected on left lane" % (self.node_name))
+                    rospy.loginfo("[%s] Duckie on left lane at %fm" % (self.node_name, duckie_loc_world.x))
 
                 i=i+1
 
@@ -199,26 +187,18 @@ class DuckieDetectionNode(DTROS):
         duckie_msg.header.stamp = rospy.Time.now()
         self.pub_duckie.publish(duckie_msg)
 
-        #if boolean is on True: publish debug images (mask and hsv image)
-        if self.publish_debugimg:
-            image_msg_out = self.bridge.cv2_to_imgmsg(cv_image_crop,"bgr8")
-            image_mask_msg_out = self.bridge.cv2_to_imgmsg(mask,"passthrough")
 
-            compress_image_msg_out = self.bridge.cv2_to_compressed_imgmsg(cv_image_crop)
-            compress_image_mask_msg_out = self.bridge.cv2_to_compressed_imgmsg(mask)
 
-            self.pub_boundingbox_image.publish(compress_image_msg_out)
-            self.pub_mask_image.publish(compress_image_mask_msg_out)
+        # image_msg_out = self.bridge.cv2_to_imgmsg(cv_image_crop,"bgr8")
+        # image_mask_msg_out = self.bridge.cv2_to_imgmsg(mask,"passthrough")
+        compress_image_msg_out = self.bridge.cv2_to_compressed_imgmsg(cv_image_crop)
+        compress_image_mask_msg_out = self.bridge.cv2_to_compressed_imgmsg(mask)
+
+        self.pub_boundingbox_image.publish(compress_image_msg_out)
+        self.pub_mask_image.publish(compress_image_mask_msg_out)
 
         elapsed_time = (rospy.Time.now() - start).to_sec()
         self.pub_time_elapsed.publish(elapsed_time)
-
-        # try:
-        #     self.trigger_led_hazard_light(
-        #         duckiefound, False
-        #     )
-        # except Exception:
-        #     self.trigger_led_hazard_light(duckiefound, False)
 
     def cbLanePose(self,lane_pose_msg): #callback to lane pose
         self.d = lane_pose_msg.d
@@ -236,46 +216,7 @@ class DuckieDetectionNode(DTROS):
         point.x = x/z
         point.y = y/z
         point.z = 0.0
-        return point
-    
-    # def trigger_led_hazard_light(self, detection, stopped):
-    #     """
-    #     Publish a service message to trigger the hazard light at the back of the robot
-    #     """
-    #     msg = String()
-
-        # if stopped:
-        #     msg.data = "OBSTACLE_STOPPED"
-        #     if msg.data != self.last_led_state:
-        #         self.changePattern(msg)
-        #     self.last_led_state = msg.data
-        # elif detection:
-        #     msg.data = "OBSTACLE_ALERT"
-        #     if msg.data != self.last_led_state:
-        #         self.changePattern(msg)
-        #     self.last_led_state = msg.data
-        # else:
-        #     if self.state == "LANE_FOLLOWING":
-        #         msg.data = "CAR_DRIVING"
-        #         if msg.data != self.last_led_state:
-        #             self.changePattern(msg)
-        #         self.last_led_state = "CAR_DRIVING"
-        #     elif self.state == "NORMAL_JOYSTICK_CONTROL":
-        #         msg.data = "WHITE"
-        #         if msg.data != self.last_led_state:
-        #             self.changePattern(msg)
-        #         self.last_led_state = msg.data
-
-        # if detection:
-        #     msg.data = "OBSTACLE_ALERT"
-        #     if msg.data != self.last_led_state:
-        #         self.changePattern(msg)
-        #     self.last_led_state = msg.data
-        # else:
-        #     msg.data = "CAR_DRIVING"
-        #     if msg.data != self.last_led_state:
-        #         self.changePattern(msg)
-        #     self.last_led_state = msg.data
+        return point     
 
 if __name__ == '__main__':
     duckie_detection_node = DuckieDetectionNode(node_name='duckie_detection_node')
--- packages/dynamic_obstacle_avoidance/src/dynamic_controller_node.py
+++ packages/dynamic_obstacle_avoidance/src/dynamic_controller_node.py
@@ -19,12 +19,6 @@ class Dynamic_Controller(DTROS):
         super(Dynamic_Controller, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         self.veh_name = rospy.get_namespace().strip("/")
 
-        # changing LED to two white in the front and two red in the back
-        self.set_led_pattern = rospy.ServiceProxy('/%s/led_emitter_node/set_custom_pattern' % self.veh_name, SetCustomLEDPattern)
-        pattern = LEDPattern()
-        pattern.color_list = ["white","red","red","white"]
-        pattern.frequency = 1.0
-        self.set_led_pattern(pattern)
 
         self.nr_steps = 4   #nr of steps to split the d_offset during transition from one lane to another
         self.transition_time = 4.0  #sec, time for transition from one lane to another
@@ -36,11 +30,6 @@ class Dynamic_Controller(DTROS):
         self.stop_prev = False
         self.overtaking = False
 
-        self.head_state = False
-        self.back_state = False
-        self.head_veh_pose = 0
-        self.back_veh_pose = 0
-
         self.duckie_left_state = False
         self.duckie_right_state = False
         self.duckie_left_pose = 0
@@ -51,25 +40,32 @@ class Dynamic_Controller(DTROS):
         self.gain = self.gain_calib
         self.gain_overtaking = 1.3 * self.gain_calib
         self.dist_max = 0.7
-        self.static = True
-        # try:
-        #     if os.environ["STATIC"]:
-        #         self.static = True
-        # except:
-        #     pass
-
-        # Log info
-        rospy.loginfo("[%s] initialized" %self.node_name)
 
         self.fsm_state = "NORMAL_JOYSTICK_CONTROL"
 
         # construct publisher
         self.sub_fsm_mode = rospy.Subscriber("~fsm_mode", FSMState, self.cbMode, queue_size=1)
         self.sub_duckie = rospy.Subscriber('/%s/duckie_detection_node/detected_duckie' %self.veh_name, dynamic_obstacle, self.cbDuckie, queue_size=1)
-        self.sub_car_cmd = rospy.Subscriber("~car_cmd_in", Twist2DStamped, self.cbCarCmd, queue_size=1)
+        self.sub_car_cmd = rospy.Subscriber("~car_cmd", Twist2DStamped, self.cbCarCmd, queue_size=1)
 
         self.car_cmd_pub = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size = 1)
         self.fsm_mode_pub = rospy.Publisher("~fsm_mode", FSMState, queue_size = 1)
+
+        # changing LED to two white in the front and two red in the back
+        self.led_pattern_srv = rospy.ServiceProxy('/%s/led_emitter_node/set_custom_pattern' %self.veh_name, SetCustomLEDPattern)
+        self.led_pattern = LEDPattern()
+        self.led_pattern.color_list = ['white', 'switchedoff', 'white', 'yellow', 'yellow'] # FL, !, FR, RR, RL
+        self.led_pattern.color_mask = [1,1,1,1,1]
+        self.led_pattern.frequency = 0.0
+        self.led_pattern.frequency_mask = [1,1,1,1,1]
+
+        self.led_state = "drive" # drive, reverse, signal_left, signal_right, stop, emergency_stop
+
+        self.led_pattern_srv(self.led_pattern)
+
+        
+
+        rospy.loginfo("[%s] initialized" %self.node_name)
 
     def cbMode(self,fsm_state_msg):
         self.fsm_state = fsm_state_msg.state    # String of current FSM state
@@ -92,8 +88,11 @@ class Dynamic_Controller(DTROS):
         else:
             self.duckie_left_pose = 0
 
-    # velocity and omega of car_cmd are set to zero if there is a emergency stop
+    # velocity and omega of car_cmd are set to zero if there is a stop
     def cbCarCmd(self, car_cmd_msg):
+
+        self.overwatch()    # execute overwatch
+
         car_cmd_msg_current = Twist2DStamped()
         car_cmd_msg_current = car_cmd_msg
         car_cmd_msg_current.header.stamp = rospy.Time.now()
@@ -102,40 +101,60 @@ class Dynamic_Controller(DTROS):
             car_cmd_msg_current.v = 0
         self.stop_prev = self.stop
         self.car_cmd_pub.publish(car_cmd_msg_current)
-        rospy.loginfo("[%s] car_cmd published" %self.node_name)
+        rospy.loginfo("[%s] car_cmd published: %f" % (self.node_name, car_cmd_msg_current.v))
 
     # function which decides when to overtake or stop
     def overwatch(self):
+
+        # ONLY FOR TESTING THE LIGHTS
+        # if self.duckie_left_state and self.duckie_left_pose > 0.3:
+        #     self.set_led_state("signal_left")
+        # elif self.duckie_right_state and self.duckie_right_pose > 0.3:
+        #     self.set_led_state("signal_right")
+        # elif self.duckie_right_pose < 0.30:
+        #     self.set_led_state("reverse")
+        # elif self.duckie_left_pose < 0.30:
+        #     self.set_led_state("emergency_stop")
+        # elif not (self.duckie_left_state or self.duckie_right_state):
+        #     self.set_led_state("drive")
+
+
         self.stop = False
-        if self.fsm_state == "LANE_FOLLOWING" and not self.overtaking:
-            if self.duckie_left_state or self.duckie_right_state:
+        self.set_led_state("drive")
+
+        if self.fsm_state == "LANE_FOLLOWING":
+            if self.duckie_right_state:
+                rospy.logerr("[%s] Stop!" % self.node_name)
                 self.stop = True
-                rospy.logerr("[%s] Emergency stop!" % self.node_name)
-                self.fsm_mode_pub.publish(FSMState(state="NORMAL_JOYSTICK_CONTROL"))
-                
+                self.set_led_state("stop")
 
+            elif self.duckie_left_state:
+                rospy.logerr("[%s] Emergency Stop!" % self.node_name)
+                self.stop = True
+                self.set_led_state("emergency_stop")
 
-
-
-            # if self.duckie_right_state: # checking for obstacles on right lane (duckiebot or duckie)
-            #     if not (self.duckie_left_state): # checking for obstacles on left lane (duckiebot or duckie)
-            #         self.leftlane_time = 2
-            #         self.dist_max = 1.0
-            #         if (self.duckie_right_pose > 0.15 and self.duckie_right_pose < self.dist_max): # checking if obstacles in overtaking range
-            #             self.overtake()
-
-            #     # checking if obstacles to close --> emergency stop
-            #     if (self.back_veh_pose < 0.15 and self.back_state) or (self.duckie_right_pose < 0.15 and self.duckie_right_state):
-            #         rospy.logerr("[%s] Emergency stop!" % self.node_name)
-            #         self.stop = True
-            #     else:
-            #         self.stop = False
+        # if self.fsm_state == "LANE_FOLLOWING" and not self.overtaking:
+        #     if self.duckie_right_state: # checking for duckies on right lane
+        #         if not (self.duckie_left_state): # checking for duckies on left lane
+        #             self.leftlane_time = 2
+        #             self.dist_max = 1.0
+        #             if (self.duckie_right_pose > 0.15 and self.duckie_right_pose < self.dist_max): # checking if duckie in overtaking range
+        #                 self.overtake()
+        #         # if self.duckie_left_state: # MAYBE IMPLEMENT 'inversao de marcha'!   
+        #         if (self.duckie_right_pose < 0.15 and self.duckie_right_state): # checking if duckie to close
+        #             rospy.logerr("[%s] Stop!" % self.node_name)
+        #             self.set_led_state("stop")
+        #             self.stop = True
+        #         else:
+        #             self.stop = False
 
     # overtaking by adjusting d_offset
     def overtake(self):
         self.overtaking = True
+        rospy.loginfo("[%s] overtaking started" % self.node_name)
+        rospy.loginfo("[%s] going to the left" % self.node_name)
+        self.set_led_state("signal_left")
 
-        rospy.loginfo("[%s] overtaking now, going left" % self.node_name)
         for i in range(1,self.nr_steps+1): # changing d_offset as a descrete sine from 0 to pi/2
             self.d_offset = math.sin(i*math.pi/(self.nr_steps*2)) * self.lanewidth
             rospy.set_param("/%s/lane_controller_node/d_offset" %self.veh_name, self.d_offset) #sets d_offset parameter
@@ -148,18 +167,19 @@ class Dynamic_Controller(DTROS):
         # checking for duckiebot head while leftlane_time (on the left lane); detected --> emergency stop
         t_start = rospy.get_rostime().secs
         while (t_start + self.leftlane_time) > rospy.get_rostime().secs:
-            if self.head_state or self.duckie_left_state: #stop if vehicle or duckie are facing us on left lane
+            if self.duckie_left_state: #stop if duckie are facing us on left lane
                 rospy.logerr("[%s] Emergency stop while overtaking!" % self.node_name)
+                self.set_led_state("emergency_stop")
                 self.stop_prev = True
                 self.stop = True
                 while self.stop_prev or self.stop:
-                    if self.head_state or self.duckie_left_state:
+                    if self.duckie_left_state:
                         self.stop = True
                     else:
                         self.stop = False
                     rospy.sleep(0.5)
 
-             #as soon as left lane is free again, we exit stop and continue driving on left lane
+            #as soon as left lane is free again, we exit stop and continue driving on left lane
             rospy.sleep(0.1)
 
         # decrease speed, atm set_param disabled because lanefollowing cant cope with it
@@ -167,6 +187,8 @@ class Dynamic_Controller(DTROS):
         # rospy.set_param("/%s/kinematics_node/gain" %self.veh_name, self.gain)
 
         rospy.loginfo("[%s] going back to the right" % self.node_name)
+        self.set_led_state("signal_right")
+
         for i in range(1,self.nr_steps+1):   # changing d_offset back
             self.d_offset = (1 - math.sin(i*math.pi/(self.nr_steps*2)) )* self.lanewidth
             rospy.set_param("/%s/lane_controller_node/d_offset" %self.veh_name, self.d_offset)
@@ -176,10 +198,56 @@ class Dynamic_Controller(DTROS):
         rospy.loginfo("[%s] overtaking done" % self.node_name)
         self.overtaking = False
 
+    # Setting the led state has some delay!
+    def set_led_state(self, new_led_state):
+        if new_led_state != self.led_state:
+            if new_led_state == "drive":
+                self.led_pattern.color_list = ['white', 'switchedoff', 'white', 'yellow', 'yellow']
+                self.led_pattern.color_mask = [1,1,1,1,1]
+                self.led_pattern.frequency = 0.0
+                self.led_pattern.frequency_mask = [1,1,1,1,1]
+                self.led_pattern_srv(self.led_pattern)
+                self.led_state = "drive"
+            elif new_led_state == "reverse":
+                self.led_pattern.color_list = ['white', 'switchedoff', 'white', 'pink', 'pink']
+                self.led_pattern.color_mask = [1,1,1,1,1]
+                self.led_pattern.frequency = 0.0
+                self.led_pattern.frequency_mask = [1,1,1,1,1]
+                self.led_pattern_srv(self.led_pattern)
+                self.led_state = "reverse"
+            elif new_led_state == "signal_left":
+                self.led_pattern.color_list = ['yellow', 'switchedoff', 'white', 'yellow', 'yellow']
+                self.led_pattern.color_mask = [1,1,1,1,1]
+                self.led_pattern.frequency = 1.0
+                self.led_pattern.frequency_mask = [1,0,0,0,1]
+                self.led_pattern_srv(self.led_pattern)
+                self.led_state = "signal_left"
+            elif new_led_state == "signal_right":
+                self.led_pattern.color_list = ['white', 'switchedoff', 'yellow', 'yellow', 'yellow']
+                self.led_pattern.color_mask = [1,1,1,1,1]
+                self.led_pattern.frequency = 1.0
+                self.led_pattern.frequency_mask = [0,0,1,1,0]
+                self.led_pattern_srv(self.led_pattern)
+                self.led_state = "signal_right"
+            elif new_led_state == "stop":
+                self.led_pattern.color_list = ['white', 'switchedoff', 'white', 'red', 'red']
+                self.led_pattern.color_mask = [1,1,1,1,1]
+                self.led_pattern.frequency = 0.0
+                self.led_pattern.frequency_mask = [1,1,1,1,1]
+                self.led_pattern_srv(self.led_pattern)
+                self.led_state = "stop"
+            elif new_led_state == "emergency_stop":
+                self.led_pattern.color_list = ['red', 'switchedoff', 'red', 'red', 'red']
+                self.led_pattern.color_mask = [1,1,1,1,1]
+                self.led_pattern.frequency = 1.0
+                self.led_pattern.frequency_mask = [1,0,1,1,1]
+                self.led_pattern_srv(self.led_pattern)
+                self.led_state = "emergency_stop"
+
 
     def run(self):
         # run at 50Hz
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(50) # Previous value was 50! (in case needed for debugging)
         while not rospy.is_shutdown():
             self.overwatch()    # execute overwatch
             # rospy.loginfo("FSM STATE: %s" %self.fsm_state)
@@ -188,6 +256,6 @@ if __name__ == '__main__':
     # create the node
     node = Dynamic_Controller(node_name='dynamic_controller_node')
     # run node
-    node.run()
+    # node.run()
     # keep spinning
     rospy.spin()
